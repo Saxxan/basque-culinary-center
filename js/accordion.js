7f97a3fc2a9ac5d2a1344996172dac48dedf9906
Laupagom <laura.parrenogomez@gmail.com>
2022-07-28 08:30:48 +0200

    accordion-js

--- js/accordion.js
+++ js/accordion.js
@@ -1,15 +1,27 @@
 // Accordion
-const acc = document.getElementsByClassName("accordion");
-const panel = document.getElementsByClassName("panel");
+const acc = document.querySelectorAll(".accordion");
+const panel = document.querySelectorAll(".panel");
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.minHeight) {
-        panel.style.minHeight = null;
+acc.forEach((item, key) => {
+    item.addEventListener("click", function () {
+        item.classList.toggle("active");
+        if(panel[key].classList.contains("visible")) {
+            panel[key].classList.remove("visible");
         } else {
-        panel.style.minHeight = panel.scrollHeight + "px";
+            panel[key].classList.add("visible");
         }
     });
-}
+})
+
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.minHeight) {
+//             panel.style.minHeight = null;
+//         } else {
+//             panel.style.minHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
