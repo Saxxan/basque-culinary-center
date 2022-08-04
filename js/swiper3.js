8dc16cdfd757897be2a7593893120d3b9b29a3f3
Laupagom <laura.parrenogomez@gmail.com>
2022-08-04 18:36:00 +0200

    slider-3

--- js/swiper3.js
+++ js/swiper3.js
@@ -1,8 +1,12 @@
 var swiper3 = new Swiper(".swiper3", {
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
   loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
   breakpoints: {
     280: {
         slidesPerView: 1,
@@ -10,12 +14,12 @@ var swiper3 = new Swiper(".swiper3", {
         slidesPerGroup: 1,
     },
     500: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 15,
         slidesPerGroup: 1,
     },
     1081: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 15,
         slidesPerGroup: 1,
     },
