0ed3e5938ccf3e239c9d278839571465468c544f
Laupagom <laura.parrenogomez@gmail.com>
2022-08-17 19:07:24 +0200

    migas-de-pan

--- js/migas.js
+++
@@ -1,8 +0,0 @@
-// Migas de pan menu-Selection.pug
-function setSeparadorMigasPan()
-{
-    var migas = document.getElementByClassName('migas').getElementsByTagName('li');
-    for(i=1;i<migas.length;i++){
-        migas[i].innerHTML = "/ "+migas[i].innerHTML;
-    }
-}
